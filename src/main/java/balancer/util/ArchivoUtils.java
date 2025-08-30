35964cb62f6a5c124a37da542eab78703dbfe169
Juan José Ortiz Loaiza <juanj.ortizl@uqvirtual.edu.co>
2025-08-30 09:06:29 -0500

    Use user home for data directory

--- src/main/java/balancer/util/ArchivoUtils.java
+++ src/main/java/balancer/util/ArchivoUtils.java
@@ -1,14 +1,16 @@
 package balancer.util;
 
 import java.io.File;
+import java.nio.file.Path;
 
 public final class ArchivoUtils {
-    public static final String BASE_DIR = "C:" + File.separator + "Balancer";
-    public static final String CONFIG_DIR = BASE_DIR + File.separator + "config";
-    public static final String USUARIOS_DIR = BASE_DIR + File.separator + "usuarios";
-    public static final String PUNTOS_DIR = BASE_DIR + File.separator + "puntos";
-    public static final String CUADRES_DIR = BASE_DIR + File.separator + "cuadres";
-    public static final String LOGS_DIR = BASE_DIR + File.separator + "logs";
+    public static final String BASE_DIR =
+            Path.of(System.getProperty("user.home"), "Balancer").toString();
+    public static final String CONFIG_DIR = Path.of(BASE_DIR, "config").toString();
+    public static final String USUARIOS_DIR = Path.of(BASE_DIR, "usuarios").toString();
+    public static final String PUNTOS_DIR = Path.of(BASE_DIR, "puntos").toString();
+    public static final String CUADRES_DIR = Path.of(BASE_DIR, "cuadres").toString();
+    public static final String LOGS_DIR = Path.of(BASE_DIR, "logs").toString();
     private ArchivoUtils(){}
     public static void inicializar(){
         crear(CONFIG_DIR); crear(USUARIOS_DIR); crear(PUNTOS_DIR); crear(CUADRES_DIR); crear(LOGS_DIR);
